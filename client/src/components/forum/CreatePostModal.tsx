2dfe48c35eabbeffbe0f62f0aff434a242e2f0fe
mmckeon16 <mmckeon204@gmail.com>
2025-05-21 01:03:52 -0400

    fixing some a11y

--- client/src/components/forum/CreatePostModal.tsx
+++ client/src/components/forum/CreatePostModal.tsx
@@ -197,8 +197,19 @@ export default function CreatePostModal({ onClose, categoryId }: CreatePostModal
             
             {includePoll && (
               <div className="mb-4 bg-gray-800 p-4 rounded-lg">
+                <div className="flex justify-between items-center mb-3">
+                  <label htmlFor="poll-question" className="block text-gray-300 font-medium">Poll Question</label>
+                  <button 
+                    type="button"
+                    onClick={() => setIncludePoll(false)}
+                    className="text-gray-400 hover:text-white"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    </svg>
+                  </button>
+                </div>
                 <div className="mb-3">
-                  <label htmlFor="poll-question" className="block text-gray-300 mb-2 font-medium">Poll Question</label>
                   <input 
                     type="text" 
                     id="poll-question" 
@@ -237,7 +248,7 @@ export default function CreatePostModal({ onClose, categoryId }: CreatePostModal
                   <button 
                     type="button"
                     onClick={handleAddPollOption}
-                    className="flex items-center text-ufc-blue hover:bg-ufc-blue-dark text-sm font-medium mt-2"
+                    className="flex items-center text-ufc-blue hover:text-white text-sm font-medium mt-2"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />

d099422f80900c88ba5552b628f0b5bda15b990f
ngattusohw <ngattusohw@gmail.com>
2025-08-12 15:24:13 -0400

    feat: ✨ integrate PostHog analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,8 +6,13 @@ import App from "./App";
 import { queryClient } from "./lib/queryClient";
 import "./index.css";
 import "./lib/fonts.css";
+import { PostHogProvider } from "posthog-js/react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
@@ -22,7 +27,24 @@ createRoot(document.getElementById("root")!).render(
     afterSignOutUrl='/'
   >
     <QueryClientProvider client={queryClient}>
-      <App />
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={{
+          api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+          capture_exceptions: true,
+          debug: import.meta.env.MODE === "development",
+          enable_recording_console_log: true,
+          session_recording: {
+            maskAllInputs: false,
+            maskInputOptions: {
+              password: true,
+            },
+            recordCrossOriginIframes: false,
+          },
+        }}
+      >
+        <App />
+      </PostHogProvider>
     </QueryClientProvider>
   </ClerkProvider>,
 );

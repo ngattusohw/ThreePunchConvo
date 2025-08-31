ffb7f81b2a603b16e7243f27e0ec35b05db9a69d
ngattusohw <ngattusohw@gmail.com>
2025-08-30 22:41:07 -0400

    feat: ✨ update email service integration with accounts@3punchconvo email

--- server/helper/emailjs.ts
+++ server/helper/emailjs.ts
@@ -1,22 +1,24 @@
-import emailjs from '@emailjs/nodejs';
-import { EmailTemplateParams, EmailResponse } from '../../shared/types';
+import emailjs from "@emailjs/nodejs";
+import { EmailTemplateParams, EmailResponse } from "../../shared/types";
 
-const sendEmail = async (templateParams: EmailTemplateParams): Promise<EmailResponse> => {
+const sendEmail = async (
+  templateParams: EmailTemplateParams,
+): Promise<EmailResponse> => {
   try {
     const response = await emailjs.send(
-      'service_cbw5dou', 
-      'template_n1jjg7t', 
-      templateParams, 
+      "service_k9zgbxi",
+      "template_ugkn49h",
+      templateParams,
       {
         publicKey: process.env.EMAILJS_PUBLIC_KEY,
         privateKey: process.env.EMAILJS_PRIVATE_KEY,
-      }
+      },
     );
-    
-    console.log('SUCCESS!', response.status, response.text);
+
+    console.log("SUCCESS!", response.status, response.text);
     return response;
   } catch (err) {
-    console.log('FAILED...', err);
+    console.log("FAILED...", err);
     throw err;
   }
 };
